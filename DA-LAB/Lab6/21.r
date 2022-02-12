39eb9441bf7c70c6e471e5c629b7350278be3cf6
rudrashisdutta <rudra2001official@gmail.com>
2022-02-12 12:02:39 +0530

    DA Lab6 Updated.
    12-02-2022 12-02p

--- DA-LAB/Lab6/21.r
+++ DA-LAB/Lab6/21.r
@@ -2,3 +2,5 @@
 # one of which is to take a vector as its input
 # and create a square matrix with that vector on the diagonal. Create a 21-by-21
 # matrix with the sequence 10 to 0 to 11 (i.e., 11, 10, ... , 1, 0, 1, ..., 11).
+x <- diag(abs(-11:11), 21, 21)
+print(x)
